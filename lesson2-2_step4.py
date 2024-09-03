259cc627b998ba879d9f94f1cad186e3d4fd5e9c
skezzerino <semyon.zuev@inbox.ru>
2024-09-03 19:16:01 +0300

    added module folders

--- lesson2-2_step4.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-import time
-
-browser = webdriver.Chrome()
-browser.execute_script("alert('Robots at work');")
-
-time.sleep(5)
---
+++ module_3/lesson3-2_step13.py
@@ -0,0 +1,46 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class lesson_tests(unittest.TestCase):
+    def test_one(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+        input3.send_keys("pochta@test.ru")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        # assert "Congratulations! You have successfully registered!" == welcome_text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Текст должен совпадать")
+        browser.quit()
+
+    def test_two(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+        input3.send_keys("pochta@test.ru")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        # assert "Congratulations! You have successfully registered!" == welcome_text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Текст должен совпадать")
+        browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
